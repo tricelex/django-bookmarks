32f4637eed3318d2228349ba21324fb05054dfdb
Chuckz Okoye <tricelex@gmail.com>
2020-04-27 17:38:03 +0100

    added functionality for resetting passwords

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+
+LOGIN_URL = 'login'
+
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
